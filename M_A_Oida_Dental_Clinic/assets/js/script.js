e7023cba7ab8d584a10c97fb104e58c85cffa960
Leih28 <147581506+Leih28@users.noreply.github.com>
2025-05-06 23:03:26 +0800

    Add files via upload

--- M_A_Oida_Dental_Clinic/assets/js/script.js
+++ M_A_Oida_Dental_Clinic/assets/js/script.js
@@ -37,3 +37,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+function togglePassword(inputId) {
+    const passwordInput = document.getElementById(inputId);
+    const eyeIcon = document.getElementById(inputId + '-eye');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+}
